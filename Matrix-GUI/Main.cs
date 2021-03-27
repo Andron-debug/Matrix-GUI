e45d1cf4a7d27a4f90dda6337cd799ee66974973
Andron-debug <b.a.a.2002@yandex.ru>
2021-03-27 15:01:53 +0300

    Версия из интернета

--- Matrix-GUI/Main.cs
+++ Matrix-GUI/Main.cs
@@ -50,50 +50,64 @@ namespace Matrix_GUI
                 s = "";
          
             }
-            int[,] MatrixInput(int type)
+        }
+        private int[,] MatrixInput(int type)
+        {
+            //Параметр type отвечает за то, откуда взять матрицу
+            //0 - A
+            //1 - B
+            //2 - C
+            TextBox[] textboxs = { Matrix1_textbox, Matrix2_textbox, Result };
+            //https://www.cyberforum.ru/windows-forms/thread1323790.html
+            int[] mas = textboxs[type].Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n.Trim())).ToArray();
+            int a;
+            int b;
+            if (type == 0)
             {
-                //Параметр type отвечает за то, откуда взять матрицу
-                //0 - A
-                //1 - B
-                //2 - C
-                TextBox[] textboxs = { Matrix1_textbox, Matrix2_textbox, Result};
-                int a = textboxs[type].Lines.GetLength(0);
-                int b = textboxs[type].Lines.GetLength(1);
-                int[,] matrix = new int[a,b];
-                for (int i = 0; i < b; i++)
-                {
-                    
-                }
-                return matrix;
+                a = n;
+                b = m;
             }
-
+            else
+            {
+                a = k;
+                b = l;
+            }
+            int[,] matrix = new int[a, b];
+            for (int i = 0; i < a; ++i)
+                for (int j = 0; j < b; ++j)
+                {
+                    matrix[i, j] = mas[i];
+                }
+            matrix_print(matrix);
+            return matrix;
         }
         private void Start_Click(object sender, EventArgs e)
         {
             n = Convert.ToInt32(textBox1.Text);
             m = Convert.ToInt32(textBox2.Text);
-            k = Convert.ToInt32(textBox3.Text);
-            l = Convert.ToInt32(textBox4.Text);
+            //k = Convert.ToInt32(textBox3.Text);
+            //l = Convert.ToInt32(textBox4.Text);
             A = new int[n, m];
-            B = new int[k, l];
+            //B = new int[k, l];
+            A = MatrixInput(0);
 
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    A[i, j] = rnd.Next(-10, 10);
-                }
-            }
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        A[i, j] = rnd.Next(-10, 10);
+            //    }
+            //}
 
-            for (int i = 0; i < k; i++)
-            {
-                for (int j = 0; j < l; j++)
-                {
-                    B[i, j] = rnd.Next(-10, 10);
-                }
-            }
-            matrix_print(A, 0);
-            matrix_print(B, 1);
+            //for (int i = 0; i < k; i++)
+            //{
+            //    for (int j = 0; j < l; j++)
+            //    {
+            //        B[i, j] = rnd.Next(-10, 10);
+            //    }
+            //}
+            //matrix_print(A, 0);
+            //matrix_print(B, 1);
         }
 
         private void Main_Load(object sender, EventArgs e)
